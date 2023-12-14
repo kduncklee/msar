3785866b04329af2865ea0df755ce43074dec44d
Kevin Duncklee <github.kevin@duncklee.net>
2023-12-13 18:45:24 -0800

    Include profile_email in password reset set

--- main/views/auth_views.py
+++ main/views/auth_views.py
@@ -1,8 +1,9 @@
 import django.contrib.auth.forms
-from main.models import Email
+from main.models import Email, Member
 
 class PasswordResetForm(django.contrib.auth.forms.PasswordResetForm):
     def get_users(self, email):
         emails = Email.objects.filter(address__iexact=email)
-        users = [e.member for e in emails]
+        users = set(Member.objects.filter(profile_email__iexact=email))
+        users.update([e.member for e in emails])
         return [u for u in users if u.is_active and u.has_usable_password()]
